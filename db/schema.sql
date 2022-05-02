23c695c6fa796b4e7350aa0f795ae191d65e0d78
Zeitel42 <43343991+Zeitel42@users.noreply.github.com>
2022-05-02 18:30:03 -0400

    added queries to schema to create tables and to seeds to populate tables

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE department(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(50) NOT NULL
+  );
+
+CREATE TABLE roles(
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL
+);
+
+CREATE TABLE employees (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  roles_id INTEGER NULL,
+  manager_id INTEGER NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,33 @@
+
+
+INSERT INTO department
+  (department_name)
+VALUES
+  ('Operations'),
+  ('Human Resources');
+  
+
+INSERT INTO roles
+  (title, salary, department_id)
+VALUES
+('Supervisor', 75000.00, 1),
+('Maintenance', 50000.00, 1),
+('Operator', 65000.00, 1),
+('Warehouse', 60000.00, 1),
+('Utility Mechanic',40000.00, 1),
+('Office Assistant', 40000.00, 2),
+('Generalist', 50000.00, 2);
+
+INSERT INTO employees
+(first_name, last_name, roles_id, manager_id)
+VALUES
+  ('Ronald', 'Firbank', 1, 2),
+  ('Virginia', 'Woolf', 1, 2),
+  ('Piers', 'Gaveston', 2, 0),
+  ('Charles', 'LeRoi', 3, 1),
+  ('Katherine', 'Mansfield', 4, 1),
+  ('Dora', 'Carrington', 5, 0),
+  ('Edward', 'Bellamy', 6, 0),
+  ('Montague', 'Summers', 7, 1),
+  ('Octavia', 'Butler', 3, 1),
+  ('Unica', 'Zurn', NULL, 1);
